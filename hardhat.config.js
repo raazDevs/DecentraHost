42d2519e98e38c185436a9c49c92c4857b051b8d
Sandeep Kumar <104671195+PARADOX-12@users.noreply.github.com>
2024-11-22 14:57:29 +0530

    Creat the smart contract

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+const fs = require("fs");
+const privateKey = fs.readFileSync("secrete.txt").toString();
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+      chainId: 4202,
+    },
+
+    BitTorrent: {
+      url: "https://pre-rpc.bt.io/",
+      accounts: [privateKey],
+      gasPrice: 1000000000,
+    },
+  },
+  solidity: "0.8.24",
+  allowUnlimitedContractSize: true,
+  throwOnTransactionFailures: true,
+  throwOnCallFailures: true,
+  loggingEnabled: true,
--- utils/db/actions.ts
+++ utils/db/actions.ts
@@ -2,6 +2,8 @@ import { db } from "./dbConfig";
 
 import { Users, Webpages,  Deployments } from "./schema";
 import { eq, sql, desc } from "drizzle-orm";
+import { create } from "@web3-storage/w3up-client";
+import { ethers } from "ethers";
 
 export async function createOrUpdateUser(address: string, email: string) {
     try {
@@ -47,4 +49,32 @@ export async function createOrUpdateUser(address: string, email: string) {
       console.error("Error creating or updating user:", error);
       return null;
     }
+  }
+
+  let web3StorageClient: any;
+
+  export async function initializeClients(userEmail: string) {
+    web3StorageClient = await create();
+  
+    // Authenticate and select a space using the user's email
+    await web3StorageClient.login(userEmail);
+    const spaces = await web3StorageClient.spaces();
+    if (spaces.length > 0) {
+      await web3StorageClient.setCurrentSpace(spaces[0].did());
+    } else {
+      throw new Error("No spaces available. Please create a space first.");
+    }
+  
+    const provider = new ethers.providers.JsonRpcProvider(
+      "https://rpc.bittorrentchain.i"
+    );
+    const signer = new ethers.Wallet(
+      "2f34d72c40a47e574ed54bbd14723d7753e8cf53434a180f67ef2f73187ef811",
+      provider
+    );
+    contract = new ethers.Contract(
+      "0x4eEAEB9C96951Fc1BE43f34c42A002B58FB774Ff",
+      WebpageStorageABI.abi,
+      signer
+    );
   }
